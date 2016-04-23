de7b246efd9024ec9548524e164062490e2cbd78
2012399 <2012399@UK2054832W2>
2016-04-23 21:45:09 +0100

    moved projects around

---
+++ AnalysisToolkit/AnalysisToolkit.BackEnd.IO/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace AnalysisToolkit.BackEnd.IO
+{
+    public class Class1
+    {
+        System.Data.SQLConnection a;
+    }
+}
